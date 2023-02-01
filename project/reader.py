aa086d6c291550c95d392f93fe7581ce29eb38f1
mmarimmasca <122800309+mmarimmasca@users.noreply.github.com>
2023-02-01 12:00:58 +0100

    Update reader.py
    Removed line of comment

--- project/reader.py
+++ project/reader.py
@@ -1,4 +1,4 @@
-##reader class for project Advanced programming
+##Reader class for project Advanced programming
 
 import pandas as pd
 from abc import ABC, abstractmethod
@@ -15,8 +15,5 @@ class Reader(ABC):
 class Reader_gff3(Reader):
     @staticmethod
     def read(file):
-        df = pd.read_table(file, delimiter="\t",
-                           names=["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"])
-        # invece di pulire il df in Dataset, possiamo pulirlo direttamente quando lo leggiamo con : skiprows, quotechar
-
+        df = pd.read_table(file, delimiter="\t", names=["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"])
         return Dataset_gff3(df)
