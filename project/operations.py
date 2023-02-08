1c1802943dea819b3e94b8b7d484c30703b839cd
mmarimmasca <122800309+mmarimmasca@users.noreply.github.com>
2023-02-08 11:10:23 +0100

    Update operations.py
    added comments (to be completed !)

--- project/operations.py
+++ project/operations.py
@@ -1,40 +1,53 @@
-##decorator and operations project Advanced programming
+# Advanced Programming project
+# Decorator @isactive, Operation class
 
 import pandas as pd
 from dataset import DataSet, DataSetGff3
 
-# DECORATOR
-# import inspect
-
-global_l = ['columnInfo', 'listSeqIds', 'listTypes', 'countFeaturesInSource', 'countEntriesForType',
-            'infoEntireChroms', 'fractionUnassembled', 'newHavana', 'countEntriesNewHavana', 'geneName']
+''' The 'global_l' variable lists the active operations among the registry of operations.
+    Therefore, 'global_l' is a list which elements are the names of the methods currently active. '''
+global_l = ['columnInfo', 'listSeqIds', 'listTypes', 'countFeaturesInSource', 'countEntriesForType', 'infoEntireChroms', 'fractionUnassembled', 'newHavana', 'countEntriesNewHavana', 'geneName']
 
 
+# Function splitting rows
 def split_rows(row):
+    ''' The function 'split_rows' is needed in the 'geneName' method of the class Operation.
+        It takes as input a row of the dataset (???) and returns ??? '''
     return str(row).split(';')[1].split('=')[1]
-    
-    
+
+# Decorator @isactive
+''' The '@isactive' decorator allows the execution of the method on the input dataset of the active operations only.
+    In practice, it checks whether the name of the method to which is applied is present in the list of active operations (global_l) or not.
+    In the case it's present, it executes the method on the dataset, otherwise it returns an error message. '''
 def isactive(f):
     def wrapper(*args):
-        if f.__name__ in global_l:
-            return f(*args)
-        else:
-            return ('Function not in the registry of active operations')
-
+        if f.__name__ in global_l: # if the name of the function is present in global_l ...
+            return f(*args)        # ... the method is executed
+        else:                      # otherwise ...
+            return ('Function not in the registry of active operations') # ... the error message is returned 
     return wrapper
 
 
+# Operation class
 class Operation:
+    ''' The Operation class contains a series of static methods, each one implementing on the insights over the dataset required.
+        Each of the methods is decorated both with the @staticmethod decorator (for making it static) and with the @isactive decorator.
+        Each of the methods takes as input only a parameter ('df'), which must be of the DataSet type, and returns another DataSet object, containing the results of the insight.
+        Finally, the first line of code of each method is the same and it re-sets the value of 'df' through the DataSet.getDataFrame() method,
+        so that the type of 'df' passes from DataSet to Pandas DataFrame and therefore the DataFrame's methods can be used to perform the operation on the dataset. '''
     # 1
     @staticmethod
     @isactive
     def columnInfo(df : DataSet):
+        ''' This method returns a two-column dataset where the names of the columns and the respective type are listed. '''
+        df = df.getDataFrame()
         return DataSet(pd.DataFrame(data={'names of columns': df.columns, 'types': [df[column].dtype for column in df.columns ]}).reset_index(drop=True))
 
     # 2
     @staticmethod
     @isactive
     def listSeqIds(df : DataSet):
+        ''' This method returns a single-column dataset where the single features present in the 'seqid' column are listed. '''
         df = df.getDataFrame()
         return DataSet(pd.DataFrame({'seqIDs': df['seqid'].unique()}))
 
@@ -42,6 +55,7 @@ class Operation:
     @staticmethod
     @isactive
     def listTypes(df : DataSet):
+        ''' This method returns a single-column dataset where the single features present in the 'type' column are listed. '''
         df = df.getDataFrame()
         return DataSet(pd.DataFrame({'types': df['type'].unique()}))
 
@@ -49,6 +63,7 @@ class Operation:
     @staticmethod
     @isactive
     def countFeaturesInSource(df : DataSet):
+        ''' This method returns a two-column dataset where the single features of the 'source' column and the respective count of entries are listed. '''
         df = df.getDataFrame()
         group_source = df.groupby('source')
         return DataSet(pd.DataFrame({'source': group_source.groups.keys(), 'source count': group_source.size().array}))
@@ -57,6 +72,7 @@ class Operation:
     @staticmethod
     @isactive
     def countEntriesForType(df : DataSet):
+        ''' This method returns a two-column dataset where the single features of the 'type' column and the respective count of entries are listed. '''
         df = df.getDataFrame() 
         group_type = df.groupby('type')
         return DataSet(pd.DataFrame({'types': group_type.groups.keys(), 'types count': group_type.size().array}))
@@ -65,6 +81,7 @@ class Operation:
     @staticmethod
     @isactive
     def infoEntireChroms(df : DataSet):
+        ''' This method returns a selection of the original dataset where only the entries in which the value of the 'source' column equal to 'GRCh38' are displayed. '''
         df = df.getDataFrame() 
         return DataSet(df.loc[lambda df: df['source'] == 'GRCh38'].reset_index(drop=True))
 
@@ -72,6 +89,8 @@ class Operation:
     @staticmethod
     @isactive
     def fractionUnassembled(df : DataSet):
+        ''' This method returns a single-column and single-row dataset; retrieving data from the dataset produced by the 'infoEntireChroms' method,
+            it's displayed the ratio between the entries where the value in the column 'type' is 'superconting' and the total entries of the 'infoEntireChroms' resulting dataset. '''
         count_df = Operation.infoEntireChroms(df).getDataFrame()
         fract_df = count_df.loc[lambda df: df['type'] == 'supercontig']
         return DataSet(pd.DataFrame({'unassembled/entire chromosomes': [f"{fract_df.shape[0]}/{count_df.shape[0]}"]}))
@@ -80,6 +99,7 @@ class Operation:
     @staticmethod
     @isactive
     def newHavana(df : DataSet):
+        ''' This method returns a selection of the original dataset where only the entries in which the values of the 'source' column are either 'ensembl', 'havana' or 'ensembl_havana' are displayed. '''
         df = df.getDataFrame()
         new_df = pd.DataFrame(df[df['source'].isin(['ensembl', 'havana', 'ensembl_havana'])])
         return DataSet(new_df.reset_index(drop=True))
@@ -88,6 +108,8 @@ class Operation:
     @staticmethod
     @isactive
     def countEntriesNewHavana(df : DataSet):
+        ''' This method returns a two-column dataset where data are retrieved by the dataset resulting from the 'newHavana' method.
+            The dataset lists the single features of the 'type' column and the respective number of entries for each feature. '''
         df_havana = Operation.newHavana(df).getDataFrame()
         group_type = df_havana.groupby('type')
         return DataSet(pd.DataFrame({'type': group_type.groups.keys(), 'count': group_type.size().array}))
@@ -96,9 +118,9 @@ class Operation:
     @staticmethod
     @isactive
     def geneName(df : DataSet):
+        ''' This method returns a single-column dataset where data are retrieved by the dataset resulting from the 'newHavana' method.
+            The dataset lists ???? '''
         df_havana = Operation.newHavana(df).getDataFrame()
         genes = df_havana[df_havana['type'] == 'gene']['attributes']
         names = pd.DataFrame({'genes': genes.apply(split_rows)})
         return DataSet(names.reset_index(drop=True))
-        
-        
