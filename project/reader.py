6c636153d3b74b1aca439548cfd274e279c5286d
alessiarr <122802017+alessiarr@users.noreply.github.com>
2023-02-08 16:22:37 +0100

    Update reader.py

--- project/reader.py
+++ project/reader.py
@@ -7,9 +7,9 @@ from dataset import DataSet, DataSetGff3
 
 
 class Reader(ABC):
-    ''' The Reader abstract class defines generally the procedure for reading a dataset '''
-    @staticmethod     # decorating the 'read' method as a static method
-    @abstractmethod   # decorating the 'read' method as an abstract method
+    ''' The Reader abstract class defines generally the procedure for reading a generic tabular data '''
+    @staticmethod    
+    @abstractmethod  
     def read(file):
         pass
 
@@ -20,6 +20,4 @@ class ReaderGff3(Reader):
     def read(file):
         ''' The 'read' method takes as input a file (GFF3), creates a Pandas DataFrame out of it and returns that as an instance of the DataSetGff3 class '''
         df = pd.read_table(file, delimiter="\t", names=["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"])
-            # the 'read_table' method takes the file, uses the tabular space for delimiting the columns of the dataset and assignes to each column an element of the list 'names'
-            # the output is assigned to the 'df' variable, which is a Pandas DataFrame
-        return DataSetGff3(df) # the returned value is the dataset created as an instance of the class DataSetGff3
+        return DataSetGff3(df) 
